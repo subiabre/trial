7a311705164733fbd7dc6070e766266b20a87337
Daniel <subiabrewd@gmail.com>
2022-06-15 10:01:49 +0200

    Generate personas more positive or negative inclined

--- src/routes/generate.ts
+++ src/routes/generate.ts
@@ -12,37 +12,22 @@ function randomElementsFromArray(array: any[], n = 1)
 
 function randomTraits()
 {
-    const traitsPositive = randomElementsFromArray(traits.positive, randomIntBetween(0, 3));
-    const traitsNeutral = randomElementsFromArray(traits.neutral, randomIntBetween(1, 2));
-    const traitsNegative = randomElementsFromArray(traits.negative, randomIntBetween(0, 3));
+    const [positiveRange, negativeRange] = randomIntBetween(0, 1) > 0 ? [3, 0] : [0, 3];
 
-    return [...traitsPositive, ...traitsNeutral, ...traitsNegative];
+    return [
+        ...randomElementsFromArray(traits.positive, positiveRange),
+        ...randomElementsFromArray(traits.neutral, randomIntBetween(1, 2)),
+        ...randomElementsFromArray(traits.negative, negativeRange)
+    ];
 }
-
-/**
- * @see https://github.com/bryc/code/blob/master/jshash/experimental/cyrb53.js
- */
-function cyrb53(str: string) {
-    let seed = 0;
-    let h1 = 0xdeadbeef ^ seed, h2 = 0x41c6ce57 ^ seed;
-    for (let i = 0, ch; i < str.length; i++) {
-        ch = str.charCodeAt(i);
-        h1 = Math.imul(h1 ^ ch, 2654435761);
-        h2 = Math.imul(h2 ^ ch, 1597334677);
-    }
-    h1 = Math.imul(h1 ^ (h1>>>16), 2246822507) ^ Math.imul(h2 ^ (h2>>>13), 3266489909);
-    h2 = Math.imul(h2 ^ (h2>>>16), 2246822507) ^ Math.imul(h1 ^ (h1>>>13), 3266489909);
-    return (h2>>>0).toString(16).padStart(8)+(h1>>>0).toString(16).padStart(8);
-};
 
 /** @type {import('./__types/generate').RequestHandler} */
 export async function get() {
     const name = randomElementsFromArray(names, 1)[0];  
     const age = randomIntBetween(6, 101);
     const traits = randomTraits().join(", ");
-    const code = cyrb53(name + age + traits);
 
     return {
-        body:  { name, age, traits, code }
+        body:  { name, age, traits }
     };
 }
